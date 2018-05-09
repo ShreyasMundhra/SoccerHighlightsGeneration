bd22c583b61f4a01248a39c9855a25d0da9ae8b6
ShreyasMundhra <shreyass001@e.ntu.edu.sg>
2018-05-09 02:06:52 -0400

    always select goals and find probs of selecting other events

--- eventSelector.py
+++ eventSelector.py
@@ -1,27 +1,62 @@
 import dataProcessor as dp
-import xml.etree.ElementTree as ET
 
 class Event():
 	def __init__(self, type, secs):
 		self.type = type
 		self.secs = secs
-		if type in ['goal']:
-			self.isIncluded = True
-		else:
-			self.isIncluded = False
 
-def getAllEvents(filename):
-	eventElements = dp.getAllEventElements('match.xml')
+def getAllEvents(filename, mandatory):
+	eventElements = dp.getAllEventElements(filename)
 
-	events = []
+	selected = []
+	remaining = []
 	for element in eventElements:
 		attrib = element.attrib
 		# TODO: use (mins and secs) or minsec?
-		events.append(Event(attrib['type'], int(attrib['minsec'])))
+		event = Event(attrib['type'], int(attrib['minsec']))
+		if attrib["type"] in mandatory:
+			selected.append(event)
+		else:
+			remaining.append(event)
 
-	events = sorted(events, key=lambda x: x.secs)
-	return events
+	# events = sorted(events, key=lambda x: x.secs)
+	return selected, remaining
+
+def findRemainingNumberOfSelectedEvents(selected, eventSecs=10, highlightSecs=180):
+	return highlightSecs/eventSecs - len(selected)
+
+
+def getEventCounts(events):
+	counts = {}
+	for event in events:
+		if event.type not in counts:
+			counts[event.type] = 1
+		else:
+			counts[event.type] = counts[event.type] + 1
+
+	return counts
+
+
+def getEventProbs(selected, remaining, relImportances):
+	numRemaining = findRemainingNumberOfSelectedEvents(selected)
+	eventCounts = getEventCounts(remaining)
+
+	denominator = 0
+	for event in eventCounts.keys():
+		denominator = denominator + eventCounts[event]*relImportances[event]
+
+	probs = {}
+	for event in relImportances:
+		probs[event] = relImportances[event]*numRemaining/float(denominator)
+	return probs
 
 if __name__ == "__main__":
-	events = getAllEvents('match.xml')
-	print(len(events))
+	selected, remaining = getAllEvents('match.xml', ['goal'])
+
+	# TODO: Try changing relImportances
+	relImportances = {'Foul': 0.25, 'off_target': 0.33, 'yellow': 0.5, 'save': 1}
+	eventProbs = getEventProbs(selected, remaining, relImportances)
+
+	# TODO: Select events to be shown in highlights based on eventProbs
+	# TODO: Substitutions near beginning or end of game should be selected
+	# TODO: No two consecutive events should be of the same type
